5155af830f2a6bc8b844ac825d67d90427965336
~ <37386853+tilde-nya@users.noreply.github.com>
2023-10-05 10:21:47 +0100

    add update URL

--- minimize.user.js
+++ minimize.user.js
@@ -2,7 +2,8 @@
 // @name        Brewman v7 - Minimize Outlet Divs
 // @match       https://brewman.premiersystems.com/*
 // @grant       none
-// @version     1.1
+// @version     1.2
+// @downloadURL https://github.com/rebellion-beer-company/brewman-userscripts/blob/master/minimize.user.js?raw=true
 // @description Minimize all info boxes on outlet pages when loading - Helps finding the required information quickly easier
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -12,7 +13,6 @@ waitForKeyElements (".bm-card-title", minimizeDivs)
 
 function minimizeDivs (jNode) {
     url = window.location.pathname.split("/")
-    console.log(url)
     if (url[1] == "outlet" && url[3] == "crm") {
         jNode.click();
     }
